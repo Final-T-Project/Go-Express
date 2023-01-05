ada0ac0c1ac3bc993a5c7d309edbb0ba723e230a
AbdelkefiMariem <abdelkefimeriem@gmail.com>
2023-01-05 19:49:55 +0100

    test

--- front/Pages/Profil.js
+++ front/Pages/Profil.js
@@ -22,12 +22,12 @@ export default function Profil({navigation}) {
                            <TouchableOpacity>
                  <NativeBaseProvider>
                      <Center flex={1} px="3">
-                         <Avatar bg="green.500" source={{uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" }}  style={{width:140,height:140,borderRadius:100,marginTop:-90}}>
+                         <Avatar bg="green.500" source={{uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" }}  style={{width:140,height:140,borderRadius:100,marginTop:-600}}>
                         </Avatar>
                       </Center>
                 </NativeBaseProvider>
                            </TouchableOpacity> 
-                            <Text style={{fontSize:25,fontWeight:'bold',padding:10,color:'black'}}>User name</Text>
+                            <Text style={{fontSize:25,fontWeight:'bold',padding:0,color:'black'}}>User name</Text>
                           
                         </View>
                         {/* user number */}
@@ -35,7 +35,7 @@ export default function Profil({navigation}) {
                         style={{alignSelf:'center',
                                 flexDirection:'row',
                                 // justifyContent:'centre',
-                                backgroundColor:'#fff',
+                                backgroundColor:'red',
                                 width:'90%',
                                 padding:20,
                                 paddingBottom:22,
