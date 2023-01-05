e7c68eedd7d574c62d3bb747e041154d32271cfd
ahmed mejdoub <mejdoubahmed77@gmail.com>
2023-01-05 22:12:32 +0100

    before pull

--- front/App.js
+++ front/App.js
@@ -12,12 +12,10 @@ import Profil from "./Pages/Profil";
 import LogInSignIn from "./Pages/LogInSignIn";
 import Home from "./Pages/Home";
 import SignIn from "./Pages/SignIn";
-import AddProduct from "./Pages/AddProduct"
-
+import AddProduct from "./Pages/AddProduct";
 
 // import { PhoneAuthCredential } from 'firebase/auth';
 import PhoneNumber from "./Pages/PhoneNumber";
-import EmailConfirmation from "./components/EmailConfirmation.js";
 
 const Stack = createNativeStackNavigator();
 
@@ -52,7 +50,7 @@ export default function App() {
         />
         <Stack.Screen name="Products" component={Products} />
         <Stack.Screen name="ProductDetails" component={ProductDetails} />
-        <Stack.Screen name="Email Confiramtion" component={EmailConfirmation} />
+        {/* <Stack.Screen name="Email Confiramtion" component={EmailConfirmation} /> */}
         <Stack.Screen
           name="AddProduct"
           options={{ headerShown: false }}
