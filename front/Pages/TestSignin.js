576b3f8cebff674fa3ba375c32553da685ed679b
Fedi <fediguizeniedu@gmal.com>
2023-01-11 15:26:58 +0000

    SignIn fixing done with AsyncStorage

--- front/Pages/TestSignin.js
+++ front/Pages/TestSignin.js
@@ -21,6 +21,7 @@ import {
   onAuthStateChanged,
 } from "firebase/auth"; // importing the auth of Firebase
 import { initializeApp } from "firebase/app";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import IPADRESS from "../config/IPADRESS";
 
@@ -75,11 +76,24 @@ function TestLogin() {
             .catch((err) => {
               alert(err);
             });
+
+            AsyncStorage.setItem(
+              "userData",
+              JSON.stringify({
+                userId: userCredential.user.uid,
+              })
+            );
+            return userCredential.user.uid;
+
         })
-        .then(() => {
+
+
+        .then((id) => {
           alert("YEYYY USER ADDED");
           console.log("------>" + userId);
-          Navigation.navigate("SideBar");
+
+
+          Navigation.navigate("SideBar", { id });
 
           //Navigation.navigate("PhoneNumber Verif",{name:value.nameUser,email:value.email})
         })
