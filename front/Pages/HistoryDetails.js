cfc231bdfc4e880a3ddeb622b2b330551713a71d
AbdelkefiMariem <abdelkefimeriem@gmail.com>
2023-01-13 09:21:18 +0100

    History details

---
+++ front/Pages/HistoryDetails.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import {
+  StatusBar,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+ 
+} from 'react-native';
+import {
+
+  Image,
+  Box,
+  VStack,
+} from 'native-base';
+import TabBar from "../components/TabBar";
+const { width, height } = Dimensions.get('screen');
+const SPACING = 20;
+export default ({navigation})=>{
+    return(
+        <View  style={StyleSheet.absoluteFillObject}>
+            <View
+            style={{flexDirection: "row" , padding: SPACING , marginBottom: SPACING , backgroundColor :'rgba(255,255,255,0.8)' , borderRadius: 12 , borderColor: "red"  
+            ,borderWidth: 1.5,
+            borderRadius: 25,
+            borderColor: "#f14e24",
+            shadowColor : "#000" ,
+            shadowOffset : {
+                width:0 ,
+                height: 10
+            } ,
+            shadowOpacity: .3,
+            shadowRadius: 20 ,
+            marginTop:20,
+            }}
+            >
+<VStack>
+    <Text style={{fontSize:20 , fontWeight: '700',margintop:10 }}
+ >Booked </Text>
+  <Text style={{fontSize:15 , fontWeight: '400' }}
+ >Booked in 00/00/0000  at 00:00</Text>
+ {/* <view style={{marginBottom:50}}></view> */}
+ <Text style={{fontSize:20 , fontWeight: '700',margintop:10 }}> your commandes </Text>
+
+ </VStack>
+            </View>
+           
+            <TabBar navigation={navigation} />
+        </View>
+    )
+}
